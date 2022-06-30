07c39283fe98cd3ab5c5eb1b8630342c02427530
emmanuelikeh <tochukwuikye@gmail.com>
2022-06-30 14:48:52 -0700

    Reviews and Dialog commit
    obtained data from the foursquares' api and added it to the detailed activity. created an alert dialog box so users so users could compose invites

--- app/src/main/java/com/example/where2meet/DetailActivity.java
+++ app/src/main/java/com/example/where2meet/DetailActivity.java
@@ -1,15 +1,24 @@
 package com.example.where2meet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.RequestHeaders;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.where2meet.databinding.ActivityDetailBinding;
+import com.example.where2meet.databinding.DialogInviteBinding;
+import com.example.where2meet.databinding.DialogInviteBinding;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,7 +34,9 @@ import okhttp3.Headers;
 public class DetailActivity extends AppCompatActivity {
     private ActivityDetailBinding activityDetailBinding;
     SearchResult searchResult;
-    protected List<DetailActivity> detailActivityList;
+    protected List<Reviews> reviewList;
+    protected ReviewsAdapter adapter;
+
 
 
     @Override
@@ -33,13 +44,88 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         activityDetailBinding = ActivityDetailBinding.inflate(getLayoutInflater());
         setContentView(activityDetailBinding.getRoot());
+
         searchResult = (SearchResult) Parcels.unwrap(getIntent().getParcelableExtra(SearchResult.class.getSimpleName()));
         activityDetailBinding.tvFormattedAddress.setText("Address: " + searchResult.getFormattedAddress());
         activityDetailBinding.tvDetailName.setText("Name: " + searchResult.getName());
         String id = searchResult.getFsqId();
+
+
         String hostLink = formHostLink(id,"https://api.foursquare.com/v3/places/","/photos" );
         queryOtherDetails(hostLink);
 
+        activityDetailBinding.flBtnInvite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailActivity.this,"this works", Toast.LENGTH_SHORT).show();
+                showAlertDialog();
+
+            }
+        });
+
+        String reviewLink = formHostLink(id,"https://api.foursquare.com/v3/places/", "/tips");
+
+        reviewList = new ArrayList<>();
+        adapter = new ReviewsAdapter(this, reviewList);
+        activityDetailBinding.rvReviews.setAdapter(adapter);
+        activityDetailBinding.rvReviews.setLayoutManager(new LinearLayoutManager(DetailActivity.this));
+
+
+
+        HttpUtils.getRequest(reviewLink, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    reviewList.addAll(Reviews.fromJsonArray(jsonArray));
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d("check this ", "Failed ");
+
+            }
+        });
+
+
+
+    }
+
+    private void showAlertDialog(){
+       DialogInviteBinding dialogInviteBinding = DialogInviteBinding.inflate(getLayoutInflater());
+       AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+       alertDialogBuilder.setView(dialogInviteBinding.getRoot());
+       final AlertDialog alertDialog = alertDialogBuilder.create();
+
+
+        dialogInviteBinding.etInviteAddress.setText(searchResult.getFormattedAddress());
+        
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "SEND", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(DetailActivity.this,"you clicked okay", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        dialogInviteBinding.btnSelectFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(DetailActivity.this, FriendSearchActivity.class);
+                startActivity(i);
+            }
+        });
+        alertDialog.show();
     }
 
 
@@ -48,11 +134,9 @@ public class DetailActivity extends AppCompatActivity {
         return baselink + id + endlink;
     }
 
+
     private void queryOtherDetails(String hostLink) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestHeaders headers = new RequestHeaders();
-        headers.put("Authorization", "fsq3HapICxpWstVvgaSOAlrzGLoCv7IypLnr82Q3c0AVzDk=");
-        client.get(hostLink,headers,null, new JsonHttpResponseHandler() {
+        HttpUtils.getRequest(hostLink, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 JSONArray jsonArray = json.jsonArray;
@@ -75,7 +159,13 @@ public class DetailActivity extends AppCompatActivity {
 
             }
         });
+
     }
+
+
+
+
+
 
 
 
