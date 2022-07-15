28dc1507a759b71f47db3758953f272ee323e1f2
emmanuelikeh <tochukwuikye@gmail.com>
2022-07-15 13:05:25 -0700

    filter search commit
    allowed users to filter based on inputs

--- app/src/main/java/com/example/where2meet/activities/FriendSearchActivity.java
+++ app/src/main/java/com/example/where2meet/activities/FriendSearchActivity.java
@@ -8,17 +8,22 @@ import android.view.View;
 
 import com.example.where2meet.adapters.FriendResultAdapter;
 import com.example.where2meet.databinding.ActivityFriendSearchBinding;
+import com.example.where2meet.fragments.FilterDialog;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FriendSearchActivity extends AppCompatActivity {
 
-    private ActivityFriendSearchBinding activityFriendSearchBinding;
+    public ActivityFriendSearchBinding activityFriendSearchBinding;
     private FriendResultAdapter adapter;
     private List<ParseUser> parseUsers;
 
@@ -36,7 +41,19 @@ public class FriendSearchActivity extends AppCompatActivity {
         activityFriendSearchBinding.btnFriendSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performFriendSearch();
+                try {
+                    performFriendSearch();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        activityFriendSearchBinding.btnFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FilterDialog filterDialog = new FilterDialog();
+                filterDialog.show(getSupportFragmentManager(),"checkthis");
             }
         });
     }
@@ -58,11 +75,63 @@ public class FriendSearchActivity extends AppCompatActivity {
         });
     }
 
-    private void performFriendSearch(){
+
+    private void performFriendSearch() throws JSONException {
         String friendLook =  activityFriendSearchBinding.etFriendSearchBox.getText().toString();
         adapter.clear();
-        queryUser(friendLook);
+        querySearch(friendLook);
     }
+
+
+    private void querySearch(String friend) throws JSONException {
+        String currentUser = ParseUser.getCurrentUser().getUsername();
+        String kilometre = activityFriendSearchBinding.tvFilterInputs.getText().toString();
+        String search = activityFriendSearchBinding.tvSimilarPlaceInputs.getText().toString();
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereContains("username", friend);
+        query.whereNotEqualTo("username", currentUser);
+
+        ParseGeoPoint geoPoint = getCurrentUsersLocation(ParseUser.getCurrentUser());
+        if(geoPoint != null && !kilometre.equals("")){
+            double km = Double.parseDouble(kilometre);
+            query.whereWithinKilometers("LastRecordedLocation",geoPoint,km);
+        }
+
+        if(!search.equals("")){
+            ParseUser user = ParseUser.getCurrentUser();
+            JSONArray visitedJsonArray =   user.getJSONArray("Visited");
+            if(visitedJsonArray != null){
+                ArrayList<String> visitedArray  = convertToArray(visitedJsonArray);
+                query.whereContainedIn("Visited", visitedArray);
+            }
+        }
+
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e == null){
+                    parseUsers.addAll(objects);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+
+    private ArrayList<String> convertToArray(JSONArray visitedJsonArray) throws JSONException {
+        ArrayList<String> listdata = new ArrayList<String>();
+        for(int i=0; i< visitedJsonArray.length(); i++){
+            listdata.add(visitedJsonArray.getString(i));
+        }
+        return listdata;
+
+    }
+
+    public ParseGeoPoint getCurrentUsersLocation(ParseUser user){
+              return user.getParseGeoPoint("LastRecordedLocation");
+        }
+
+
+
 
 }
 
