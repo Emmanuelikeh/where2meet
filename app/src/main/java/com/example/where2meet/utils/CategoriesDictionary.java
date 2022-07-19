7139d4a93d8f39d785d6d19ea8196b15b3dc66d2
emmanuelikeh <tochukwuikye@gmail.com>
2022-07-19 16:44:10 -0700

    place filter utils dialog commit
    added vital files used in the formulation of the place filter dialog

---
+++ app/src/main/java/com/example/where2meet/utils/CategoriesDictionary.java
@@ -0,0 +1,28 @@
+package com.example.where2meet.utils;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class CategoriesDictionary {
+
+    public Dictionary<String, String> getCategoriesDictionary() {
+        return categoriesDictionary;
+    }
+
+    public Dictionary<String, String> categoriesDictionary;
+
+    public CategoriesDictionary(){
+        categoriesDictionary = new Hashtable<String, String>();
+        categoriesDictionary.put("Arts and Entertainment","10000");
+        categoriesDictionary.put("Business and Professional Services", "11000");
+        categoriesDictionary.put("Community and Government","12000");
+        categoriesDictionary.put("Dining and Drinking","13000");
+        categoriesDictionary.put("Event","14000");
+        categoriesDictionary.put("Health and Medicine","15000");
+        categoriesDictionary.put("Landmarks and Outdoors","16000");
+        categoriesDictionary.put("Retail","17000");
+        categoriesDictionary.put("Sports and Recreation","18000");
+        categoriesDictionary.put("Travel and Transportation","19000");
+    }
+
+}
