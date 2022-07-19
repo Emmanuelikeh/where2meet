13b3cf5eca67a957419ec16b87b95969db922e73
emmanuelikeh <tochukwuikye@gmail.com>
2022-07-19 13:19:56 -0700

    placeFilter Dialog commit
    Created a dialog that would  dialog class and it's associated layout that would present a dialog for users to select filter options to be applied to the search

--- app/src/main/java/com/example/where2meet/fragments/PlaceFilterDialog.java
+++ app/src/main/java/com/example/where2meet/fragments/PlaceFilterDialog.java
@@ -1,6 +1,30 @@
 package com.example.where2meet.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
-public class PlaceFilterDialog extends Fragment {
+import com.example.where2meet.databinding.FragmentPlacesFilterDialogBinding;
+
+public class PlaceFilterDialog extends DialogFragment {
+    private FragmentPlacesFilterDialogBinding fragmentPlacesFilterDialogBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        fragmentPlacesFilterDialogBinding = FragmentPlacesFilterDialogBinding.inflate(inflater,container,false);
+        fragmentPlacesFilterDialogBinding.btnClosePlacesDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return fragmentPlacesFilterDialogBinding.getRoot();
+    }
 }
