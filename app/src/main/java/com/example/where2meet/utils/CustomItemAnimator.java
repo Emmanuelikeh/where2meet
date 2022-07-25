ed1d47f1225381461eac1c064eb1e690614b6990
emmanuelikeh <tochukwuikye@gmail.com>
2022-07-25 15:01:39 -0700

    custom animation and detail layout refractor commit
    changed structure for detail activity, override animation when items are added to a list

--- app/src/main/java/com/example/where2meet/utils/CustomItemAnimator.java
+++ app/src/main/java/com/example/where2meet/utils/CustomItemAnimator.java
@@ -10,11 +10,12 @@ import com.example.where2meet.R;
 public class CustomItemAnimator extends DefaultItemAnimator {
     @Override
     public boolean animateRemove(RecyclerView.ViewHolder holder) {
-        holder.itemView.setAnimation(AnimationUtils.loadAnimation(holder.itemView.getContext(), R.anim.fade_out));
+        holder.itemView.setAnimation(AnimationUtils.loadAnimation(holder.itemView.getContext(), R.anim.viewholder_remove_anim));
         return super.animateRemove(holder);
     }
     @Override
     public boolean animateAdd(RecyclerView.ViewHolder holder) {
+        holder.itemView.setAnimation(AnimationUtils.loadAnimation(holder.itemView.getContext(), R.anim.viewholder_add_anim));
         return super.animateAdd(holder);
     }
 }
